a753cb161f2c12a57f7966dc5931dcaae7b70f3b
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-06 12:01:10 +0100

    new updates to readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 TODO: Enter the cookbook description here.
 
+PORT 27017 not working,
+need to update test file.
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -3,10 +3,21 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-#describe port (27017) do
-#  it {should be_litsening}
-#end
+describe port(27017) do
+  it {should be_litsening}
+end
 
 describe package('mongodb') do
+  it {should be_installed}
+  it {should be_running}
   its('version') {should cmp '1:3.6.3-0ubuntu1'}
+  its('version'){should match /3\./}
 end
+
+describe service 'mongodb' do
+  it {should be_running}
+  it {should be_enabled}
+end
+
+
+#Check Service and Package
